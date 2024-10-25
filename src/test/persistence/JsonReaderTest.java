5b4d8dc95378bf94da62f357c12e859168144da9
msLchan <lila.ls.chan@hotmail.com>
2024-10-25 01:42:54 -0700

    added small generated JSON files for testing JsonReader

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,7 +1,21 @@
 package persistence;
 
+import model.Destination;
+import model.TravelBucketList;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 // Note: this code is adapted from the provided example JsonSerializationDemo
 
+class JsonReaderTest extends JsonTest {
+
+    @Test
+}
 public class JsonReaderTest {
 
 }
--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,7 +1,15 @@
 package persistence;
 
+import model.Destination;
+import model.TravelBucketList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 // Note: this code is adapted from the provided example JsonSerializationDemo
 
 public class JsonTest {
+    protected void checkDestination() {
+        assertEquals()
+    }
 
 }
