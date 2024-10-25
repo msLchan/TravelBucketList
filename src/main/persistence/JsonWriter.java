6fe18e35ab887b388234fe1d3e443a2444dfd5e9
msLchan <lila.ls.chan@hotmail.com>
2024-10-25 02:52:27 -0700

    added specification for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,7 +1,44 @@
 package persistence;
 
+import model.Destination;
+import model.TravelBucketList;
+
+import org.json.JSONObject;
+import java.io.*;
+
 // Note: this code is adapted from the provided example JsonSerializationDemo
 
+// Represents a writer that writes JSON representation of TravelBucketList to file
 public class JsonWriter {
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+    
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of TravelBucketList to file
+    public void write() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+    
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile() {
+        
+    }
 
 }
